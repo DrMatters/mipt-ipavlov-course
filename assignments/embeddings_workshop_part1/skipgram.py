2c97ce0ceb80c4a84a8791136390a15f15cfcf3a
Сёмин Кирилл <qseminq@gmail.com>
2019-03-05 22:03:57 +0300

    probably, wrong loss function fixed

--- assignments/embeddings_workshop_part1/skipgram.py
+++ assignments/embeddings_workshop_part1/skipgram.py
@@ -6,14 +6,13 @@ from nltk.corpus import stopwords
 import nltk
 
 
-
 class SkipGram(nn.Module):
     def __init__(self, vocab_size, embedding_dim):
         super(SkipGram, self).__init__()
 
         self.embedding_layer = nn.Embedding(vocab_size, embedding_dim)
         self.linear_layer = nn.Linear(embedding_dim, vocab_size, bias=False)
-        self.activation = nn.LogSoftmax(dim=0)
+        self.activation = nn.LogSoftmax(dim=1)
 
     def forward(self, inputs):
         embeds = self.embedding_layer(inputs)
@@ -39,31 +38,28 @@ class SkipGramBatcher:
             corpus = cleaned_corpus
 
         # 1. Count all word occurencies.
-        self.counted_words = Counter(corpus).most_common(self.vocab_size)
+        self._counted_words = Counter(corpus).most_common(self.vocab_size)
         # create dict using dict comprehension
-        self._idx_to_word = {idx: word for idx, (word, count) in enumerate(self.counted_words)}
-        self._word_to_idx = {word: idx for idx, (word, count) in enumerate(self.counted_words)}
+        self._idx_to_word = {idx: word for idx, (word, count) in enumerate(self._counted_words)}
+        self._word_to_idx = {word: idx for idx, (word, count) in enumerate(self._counted_words)}
 
         # append '<UNK>' token to dictionaries
         last_idx = len(self._idx_to_word)
         self._idx_to_word[last_idx] = self.unk_text
         self._word_to_idx[self.unk_text] = last_idx
-        indexed = self._words_to_indexes(corpus)
+        indexed = self.words_to_tokens(corpus, error_on_unk=False)
 
         # transform corpus from strings to indexes, to reduce memory usage
-        self.corpus_indexes = np.asarray(indexed, dtype=np.int32)
-        self.batch_shuffled_sequence = np.arange(len(self.corpus_indexes))
+        self._corpus_indexes = np.asarray(indexed, dtype=np.int32)
+        self._batch_shuffled_sequence = np.arange(len(self._corpus_indexes))
 
         # clean memory
         corpus = []
         gc.collect()
 
-    def _words_to_indexes(self, words):
-        unk_index = self._word_to_idx[self.unk_text]
-        idxes = [self._word_to_idx.get(word, unk_index) for word in words]
-        return idxes
-
     def words_to_tokens(self, words, error_on_unk=True):
+        """Function to transform iterable of words into list of tokens"""
+
         unk_index = self._word_to_idx[self.unk_text]
         idxes = [self._word_to_idx.get(word, unk_index) for word in words]
         if error_on_unk and unk_index in idxes:
@@ -71,38 +67,39 @@ class SkipGramBatcher:
         return idxes
 
     def tokens_to_words(self, tokens):
+        """Function to transfrom iterable of tokens into list of words"""
+
         words = [self._idx_to_word[token] for token in tokens]
         return words
 
-    def __iter__(self):
-        if self.shuffle_batch:
-            np.random.shuffle(self.batch_shuffled_sequence)
-        self.batch_start_pos = 0
+    def _get_random_sample(self, center_pos):
+        """Internal function to get a random sample within the selected window_size"""
 
-        return self
-
-    def get_random_sample(self, center_pos):
         left_window = np.arange(max(0, center_pos - self.window_size),
                                 center_pos)
         right_window = np.arange(center_pos + 1,
-                                 min(center_pos + self.window_size + 1, len(self.corpus_indexes)))
+                                 min(center_pos + self.window_size + 1, len(self._corpus_indexes)))
         window = np.concatenate((left_window, right_window))
         position = np.random.choice(window)
-        return self.corpus_indexes[position]
+        return self._corpus_indexes[position]
+
+    def __iter__(self):
+        if self.shuffle_batch:
+            np.random.shuffle(self._batch_shuffled_sequence)
+        self.batch_start_pos = 0
+        return self
 
     def __next__(self):
-        if self.batch_start_pos >= len(self.corpus_indexes):
+        if self.batch_start_pos >= len(self._corpus_indexes):
             raise StopIteration
         else:
-            batch_position_in_corpus = self.batch_shuffled_sequence[np.arange(
+            batch_position_in_corpus = self._batch_shuffled_sequence[np.arange(
                 self.batch_start_pos,
-                min(self.batch_start_pos + self.batch_size, len(self.batch_shuffled_sequence))
+                min(self.batch_start_pos + self.batch_size, len(self._batch_shuffled_sequence))
             )]
-            x_batch = np.asarray(self.corpus_indexes[batch_position_in_corpus])
+            x_batch = np.asarray(self._corpus_indexes[batch_position_in_corpus])
             # draw a word from window of a selected word
-            y_batch = np.asarray([self.get_random_sample(selected_word_position)
+            y_batch = np.asarray([self._get_random_sample(selected_word_position)
                                   for selected_word_position in batch_position_in_corpus]).flatten()
-            # for selected_word_position in batch_position_in_corpus:
-            #     y_batch.append(self.get_random_sample(selected_word_position))
             self.batch_start_pos += self.batch_size
             return x_batch, y_batch
